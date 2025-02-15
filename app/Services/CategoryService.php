21a89fb3f9434683bf3368fa8ee24ab8d48bf520
Trkien <tkien2408002@gmail.com>
2025-02-15 22:32:32 +0700

    ok

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -26,7 +26,7 @@ class CategoryService
                     $this->categoryTypeRepository->create([
                         'category_id' => $category->id,
                         'name' => $subcategoryName,
-                        'icon' => $data['subcategory_icons'][$index] ?? null
+                        
                     ]);
                 }
             }
@@ -58,7 +58,7 @@ class CategoryService
                 if ($subcategoryId && $existingSubcategories->has($subcategoryId)) {
                     $this->categoryTypeRepository->update([
                         'name' => $subcategoryName,
-                        'icon' => $data['subcategory_icons'][$index]
+                       
                     ], $subcategoryId);
 
                     $existingSubcategories->forget($subcategoryId);
@@ -66,7 +66,7 @@ class CategoryService
                     $this->categoryTypeRepository->create([
                         'category_id' => $category->id,
                         'name' => $subcategoryName,
-                        'icon' => $data['subcategory_icons'][$index]
+                        
                     ]);
                 }
             }
